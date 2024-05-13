e2105f873b4c3aef6a2756bd1b9aa9bd17227479
Alex Ford <Alex@CatDadCode.dev>
2024-05-13 10:39:21 -0600

    Auto-generated commit

--- src/types/Message.ts
+++
@@ -1,7 +0,0 @@
-import { User } from './User';
-
-export type Message = {
-  userId: User['id'];
-  text: string;
-  mentions?: User['id'][];
-};
--- src/types/User.ts
+++
@@ -1,5 +0,0 @@
-export type User = {
-  id: number;
-  name: string;
-  status: 'online' | 'idle' | 'busy' | 'offline';
-};
